bc7008a7cf1117040fbf69c9ae7e7bc658509d46
Mateo2131 <goz.matu@gmail.com>
2023-01-26 11:11:25 -0300

    remove darkmode and githubcomponent from header

--- src/components/Layout/Header.svelte
+++ src/components/Layout/Header.svelte
@@ -1,7 +1,5 @@
 <script>
-	import Theme from '#components/Theme/index.svelte';
 	import Auth from '#components/Auth/index.svelte';
-	import Github from '#components/Icons/Github.svelte';
 </script>
 
 <header class="border-b dark:border-b-brand-dark-border dark:bg-brand-dark-secondary">
@@ -9,13 +7,6 @@
 		<a href="/" class="text-xl font-medium">Flud</a>
 		<div class="flex items-center gap-x-3">
 			<Auth />
-			<Theme />
-			<a
-				href="https://github.com/techwithmat/flud"
-				class="py-2 px-3 rounded-md bg-gray-100 dark:bg-stone-900 hover:bg-gray-200 dark:hover:bg-stone-800 hover:transition-colors hover:duration-200"
-			>
-				<Github />
-			</a>
 		</div>
 	</nav>
 </header>
