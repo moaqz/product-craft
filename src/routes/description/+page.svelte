6fe31bc29991bfb9f6b26f4f00e7266def7a7ad7
Mateo2131 <goz.matu@gmail.com>
2023-01-28 12:58:40 -0300

    add product name & keywords fields to description page

--- src/routes/description/+page.svelte
+++ src/routes/description/+page.svelte
@@ -1,54 +1,54 @@
 <script async script lang="ts">
 	import { getDescription } from '#services/getProductDescription';
-	import Container from '#components/Form/Container.svelte';
+	import Container from '#components/Container/index.svelte';
 	import ResultCard from '#components/ResultCard/index.svelte';
 	import Loading from '#components/Icons/Loading.svelte';
 	import Button from '#components/Form/Button/index.svelte';
 	import TextArea from '#components/Form/TextArea/index.svelte';
+	import Label from '#components/Form/Label/index.svelte';
+	import Input from '#components/Form/Input/index.svelte';
+	import type { Reponse } from '#types';
 
-	let title = 'Product Descriptions';
-
-	type Reponse = {
-		id: string;
-		text: string;
-	};
-
+	let title = 'Product Description';
 	let loading = false;
 
 	let response: Reponse[];
 	async function handleSubmit(e: SubmitEvent) {
 		loading = true;
-		const { description } = Object.fromEntries(new FormData(e.target as HTMLFormElement));
+		response = [];
+		const entries = Object.fromEntries(new FormData(e.target as HTMLFormElement));
 
-		if (!description) return;
+		if (!entries) return;
 
-		response = await getDescription(description as string);
+		response = await getDescription(entries);
 		loading = false;
 	}
 </script>
 
-<Container {title}>
+<Container {title} on:submit={handleSubmit}>
 	<svelte:fragment slot="form">
-		<form class="flex flex-col gap-y-3" on:submit|preventDefault={handleSubmit}>
-			<TextArea
-				placeholder="e.g. an AI powered copywriter for small businesses and entrepreneurs..."
-				id="description"
-				label="Describe your product"
-			/>
-			{#if !loading}
-				<Button type="submit">Create Content</Button>
-			{:else}
-				<Button disabled>
-					<Loading /> Loading...
-				</Button>
-			{/if}
-		</form>
-	</svelte:fragment>
-	<svelte:fragment slot="resutls">
-		{#if response}
-			{#each response as res}
-				<ResultCard text={res?.text?.trim()} />
-			{/each}
+		<Label id="brand">Product / Brand Name</Label>
+		<Input id="brand" placeholder="e.g. Flud" required={false} />
+		<Label id="description">Describe your product</Label>
+		<TextArea
+			placeholder="e.g. an AI powered copywriter for small businesses and entrepreneurs..."
+			id="description"
+		/>
+		<Label id="keywords">Keywords</Label>
+		<Input id="keywords" placeholder="e.g. Entretaining, Friendly" />
+
+		{#if !loading}
+			<Button type="submit">Create Content</Button>
+		{:else}
+			<Button disabled>
+				<Loading /> Loading...
+			</Button>
 		{/if}
 	</svelte:fragment>
+
+	{#if response}
+		{#each response as res, idx}
+			<ResultCard text={res?.text?.trim()} {idx} />
+		{/each}
+	{/if}
 </Container>
