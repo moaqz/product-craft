8105ad6c58ce8023584e91d2764a69a1fdecaab0
techwithmat <goz.matu@gmail.com>
2023-04-12 10:42:56 -0300

    update prettier config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,16 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
-	theme: {
-		extend: {
-			colors: {
-				brand: {
-					primary: '#121212',
-					secondary: '#1e1f20',
-					border: '#414143'
-				}
-			}
-		}
-	},
-	plugins: []
+  content: ["./src/**/*.{html,js,svelte,ts}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 };
