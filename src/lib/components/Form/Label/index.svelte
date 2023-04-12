71d34cbd13ff989ba630f5829a98e0081d15f295
techwithmat <goz.matu@gmail.com>
2023-04-12 12:01:33 -0300

    remove unused components

--- src/lib/components/Form/Label/index.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-	export let id: string;
-</script>
-
-<label for={id} class="text-xl font-medium"><slot /></label>
--- src/lib/components/Result/index.ts
+++
@@ -1,3 +0,0 @@
-import ResultCard from '#components/Result/Card/index.svelte';
-
-export { ResultCard };
